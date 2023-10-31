cf7dfc36bd483dc8deebe9bd1c5a9121170373d2
Thien Toan Vu <67763741+Thientvu@users.noreply.github.com>
2023-10-30 22:03:48 -0700

    Delete project2 directory

--- project2/WebContent/payment.js
+++
@@ -1,9 +0,0 @@
-// In payment.html
-$(document).ready(function () {
-    // Retrieve total cart price from URL parameter
-    const urlParams = new URLSearchParams(window.location.search);
-    const totalPrice = urlParams.get("totalPrice");
-
-    // Display the total cart price in the payment page
-    $("#total-cart-price").text("$" + parseFloat(totalPrice).toFixed(2));
-});
